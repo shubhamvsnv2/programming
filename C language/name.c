c60734c044b741dbdf96ec8f370e67a59d320fbb
shubhamvsnv2 <72275611+shubhamvsnv2@users.noreply.github.com>
2020-10-03 00:45:49 +0530

    Update name.c
    improving code with proper commenting and getch() function

--- C language/name.c
+++ C language/name.c
@@ -1,5 +1,11 @@
+// Header file for input output functions 
 #include<stdio.h>
+
+// main function - 
+// where the execution of program begins 
 void main()
 {
+	 // prints Name 
 	printf("rahul kumar\n");
+getch();	
 }
